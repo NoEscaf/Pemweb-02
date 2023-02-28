6a763d7d45f1253e1e0339aa69a138818bb8da3b
zulhajji nasution <114472562+NoEscaf@users.noreply.github.com>
2023-02-28 11:17:52 +0700

    Add files via upload

---
+++ Praktikum 1/coba2.php
@@ -0,0 +1,7 @@
+<?php
+
+    $tims = ['fuad', 'radit', 'yasir', 'zia'];
+
+    foreach ($tims as $person) {
+        echo $person . '<br>';
+    }
---
+++ Praktikum 1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = 'zul';
+    $umur = 19;
+    $berat = 70;
+
+    echo 'nama saya : ' .   $nama;
+    echo '<br> umur saya : ' .   $umur;
+    echo '<br> berat saya : ' .  $berat;
